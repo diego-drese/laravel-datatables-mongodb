b2cda43325cd379c9ea2bec945c5a203001b5e97
Diego <diego-neumann@hotmail.com>
2025-01-24 16:09:05 -0300

    Update Role.php

--- tests/Models/Role.php
+++ tests/Models/Role.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Pimlie\DataTables\Tests\Models;
+namespace DiegoDrese\DataTables\Tests\Models;
 
-use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
-use Pimlie\DataTables\Traits\MongodbDataTableTrait;
+use MongoDB\Laravel\Eloquent\Model as Eloquent;
+use DiegoDrese\DataTables\Traits\MongodbDataTableTrait;
 
 class Role extends Eloquent
 {
