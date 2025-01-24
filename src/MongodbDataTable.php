e982da857765e888ff03cd6e0eaa5ca5b6cbd5d0
Diego <diego-neumann@hotmail.com>
2025-01-24 19:00:20 -0300

    Update MongodbDataTable.php

--- src/MongodbDataTable.php
+++ src/MongodbDataTable.php
@@ -2,15 +2,15 @@
 
 namespace DiegoDrese\DataTables;
 
-use MongoDB\Laravel\Eloquent\Model;
 use MongoDB\Laravel\Eloquent\Builder;
+use MongoDB\Laravel\Eloquent\Model;
 
 class MongodbDataTable extends MongodbQueryDataTable
 {
     /**
-     * @var \Jenssegers\Mongodb\Eloquent\Builder
+     * @var \MongoDB\Laravel\Eloquent\Builder
      */
-    protected $query;
+    protected \Illuminate\Contracts\Database\Query\Builder $query;
 
     /**
      * Can the DataTable engine be created with these parameters.
@@ -18,10 +18,10 @@ class MongodbDataTable extends MongodbQueryDataTable
      * @param mixed $source
      * @return boolean
      */
-    public static function canCreate($source)
+    public static function canCreate($source): bool
     {
         return $source instanceof Model || $source instanceof Builder ||
-            strpos(get_class($source), 'Jenssegers\Mongodb\Relations') !== false;
+            strpos(get_class($source), 'MongoDB\Laravel\Relations') !== false;
     }
 
     /**
@@ -54,7 +54,7 @@ class MongodbDataTable extends MongodbQueryDataTable
      *
      * @return string
      */
-    protected function getPrimaryKeyName()
+    protected function getPrimaryKeyName(): string
     {
         return $this->query->getModel()->getKeyName();
     }
